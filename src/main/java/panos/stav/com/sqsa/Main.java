141ef306dec8888003101e4d7bce20ae99a01fea
StavPanos <panstavropouloss@gmail.com>
2022-07-03 11:47:01 +0200

    init commit

--- src/main/java/panos/stav/com/sqsa/Main.java
+++ src/main/java/panos/stav/com/sqsa/Main.java
@@ -1,8 +1,13 @@
 package panos.stav.com.sqsa;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.Logger;
+
 public class Main {
 
+    private static Logger logger;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        logger.log(Level.INFO, "Hello World!");
     }
 }
